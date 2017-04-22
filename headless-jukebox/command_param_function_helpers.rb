cfd03583fd68e34f05136c11d26b5bd862a35c59
Bjoern Griebenow <b.griebenow@web.de>
2017-04-22 11:16:01 +0000

    Add command functions for random playback

--- headless-jukebox/command_param_function_helpers.rb
+++ headless-jukebox/command_param_function_helpers.rb
@@ -6,3 +6,95 @@ def run_cmd(command_str)
   # raise "Command failed: " + command_str if result != true
   return stdout
 end
+
+def absolute_file_entries(path)
+  Dir.entries(path).reject{|entry| entry == "." || entry == ".."}.map do |relative_filename|
+    File.join(path, relative_filename)
+  end
+end
+
+def is_audio_file(filename)
+  supported_audio_suffix = [
+    '.3gp',
+    '.aa',
+    '.aac',
+    '.aax',
+    '.act',
+    '.aiff',
+    '.amr',
+    '.ape',
+    '.au',
+    '.awb',
+    '.dct',
+    '.dss',
+    '.dvf',
+    '.flac',
+    '.gsm',
+    '.iklax',
+    '.ivs',
+    '.m4a',
+    '.m4b',
+    '.m4p',
+    '.mmf',
+    '.mogg',
+    '.mp3',
+    '.mpc',
+    '.msv',
+    '.oga',
+    '.ogg',
+    '.opus',
+    '.ra',
+    '.raw',
+    '.rm',
+    '.sln',
+    '.tta',
+    '.vox',
+    '.wav',
+    '.webm',
+    '.wma',
+    '.wv',
+  ]
+
+  supported_audio_suffix.include?(File.extname(filename))
+end
+
+def is_video_file(filename)
+  supported_video_suffix = [
+    '.3g2',  # Cell phones
+    '.3gp',  # Cell phones
+    '.amv',
+    '.asf',  # Advanced Systems Format
+    '.avi',
+    '.drc',
+    '.f4p',
+    '.f4v',
+    '.flv',
+    '.m4p',
+    '.m4v',
+    '.m4v',
+    '.mkv',
+    '.mov',
+    '.mp2',
+    '.mp4',
+    '.mpe',  # Windows 8.3 filename convention
+    '.mpeg',
+    '.mpg',
+    '.mpv',
+    '.ogg',
+    '.ogv',
+    '.qt',   # QuickTime
+    '.rm',   # Real media
+    '.rmvb', # Real media variable bitrate
+    '.svi',
+    '.vob',
+    '.webm',
+    '.wmv',  # Windows media video
+    '.yuv',  # Raw video format
+  ]
+
+  supported_video_suffix.include?(File.extname(filename))
+end
+
+def is_media_file(filename)
+  is_audio_file(filename) or is_video_file(filename)
+end
--- headless-jukebox/command_param_functions.rb
+++ headless-jukebox/command_param_functions.rb
@@ -1,4 +1,41 @@
+require 'pp'
+
 def video_feed_url_to_video_url(feed_url)
   run_cmd('curl -s ' + feed_url + ' | grep ".h264.mp4" | cut -f 2 -d "\"" |head -n 1')
     .gsub("\n", '')
+end
+
+# Scramble list order and return first item that is a media filename.
+def random_media_file_from_list(filename_list)
+  filename_list.shuffle.each do |filename|
+    puts "Drop " + + ". No media filename" if !is_media_file(filename)
+    return filename
+  end
+  raise "No media file found in list of files."
+end
+
+# Filter out all non media filenames.
+def media_file_list_from_list(filename_list)
+  filename_list.select do |filename|
+    puts "Drop " + + ". No media filename" if !is_media_file(filename)
+    is_media_file(filename)
+  end
+end
+
+# Scramble order and filter out all non media filenames.
+def random_media_file_list_from_list(filename_list)
+  media_file_list_from_list.shuffle
+end
+
+
+def random_media_file_list_in_path(path)
+  raise "Path does not exist:" + path if !File.directory?(path)
+  pp Dir.entries(path)
+  random_media_file_list_from_list(absolute_file_entries(path))
+end
+
+# Return single random media filename in path.
+def random_media_file_in_path(path)
+  raise "Path does not exist:" + path if !File.directory?(path)
+  random_media_file_from_list(absolute_file_entries(path))
 end
